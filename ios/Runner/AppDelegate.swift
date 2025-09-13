31186178b5a9e546a307845c6ea6e12749b51a7c
Tuan Minh <tuanminh2409bn@gmail.com>
2025-09-13 21:49:04 +0700

    Xoa xin quyen tracking trong info.plist

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import FBSDKCoreKit
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
-    _ application: UIApplication,
+    _application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
 
